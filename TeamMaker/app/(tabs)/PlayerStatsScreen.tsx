8de6bcaf14de1ddcf677ab58966ab259245a03f4
Jeremias Kühberger <kuehberger@student.tugraz.at>
2025-02-24 01:35:10 +0100

    added example radar chart

--- TeamMaker/app/(tabs)/PlayerStatsScreen.tsx
+++ TeamMaker/app/(tabs)/PlayerStatsScreen.tsx
@@ -1,36 +1,42 @@
 import React from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaView, View, Text, StyleSheet } from 'react-native';
+import {RadarChart} from '@salmonco/react-native-radar-chart'; 
+//It sucks that I have to use a radar chart from a random person
+//TODO maybe implement radar chart yourself?
 
-type Item = {
-    id: string;
-    name: string;
-  };
 
-const FriendsListScreen = () => {
-  // Sample data for the friends list
-  const friends = [
-    { id: '1', name: 'John Doe' },
-    { id: '2', name: 'Jane Smith' },
-    { id: '3', name: 'Alice Johnson' },
-    { id: '4', name: 'Bob Brown' },
-    { id: '5', name: 'Charlie Davis' },
+const PlayerStatsScreen = () => {
+  const data = [
+    {label: 'Speed', value: 30},
+    {label: 'Fun', value: 55},
+    {label: 'Height', value: 70},
+    {label: 'Effort', value: 35},
+    {label: 'Test1', value: 10},
+    {label: 'data6', value: 60},
+    {label: 'data7', value: 38},
+    {label: 'data8', value: 65},
   ];
 
-  // Render each friend item
-  const renderFriendItem = ({ item }: { item: Item }) => (
-    <View style={styles.friendItem}>
-      <Text style={styles.friendName}>{item.name}</Text>
-    </View>
-  );
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Friends List</Text>
-      <FlatList
-        data={friends}
-        renderItem={renderFriendItem}
-        keyExtractor={(item) => item.id}
+      <RadarChart
+        data={data}
+        maxValue={100}
+        gradientColor={{
+          startColor: '#FF9432',
+          endColor: '#FFF8F1',
+          count: 5,
+        }}
+        stroke={['#FFE8D3', '#FFE8D3', '#FFE8D3', '#FFE8D3', '#ff9532']}
+        strokeWidth={[0.5, 0.5, 0.5, 0.5, 1]}
+        strokeOpacity={[1, 1, 1, 1, 0.13]}
+        labelColor="#433D3A"
+        dataFillColor="#FF9432"
+        dataFillOpacity={0.8}
+        dataStroke="salmon"
+        dataStrokeWidth={2}
+        //isCircle
       />
     </SafeAreaView>
   );
@@ -39,23 +45,10 @@ const FriendsListScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
-    backgroundColor: '#f5f5f5',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 16,
-  },
-  friendItem: {
-    padding: 16,
-    backgroundColor: '#fff',
-    borderBottomWidth: 1,
-    borderBottomColor: '#ddd',
-  },
-  friendName: {
-    fontSize: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
-export default FriendsListScreen;
+
+export default PlayerStatsScreen;
